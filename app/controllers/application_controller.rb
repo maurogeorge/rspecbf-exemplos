fb3c45f564023946128841016cae9a42535cae61
Mauro George <maurogot@gmail.com>
2014-02-03 07:45:23 -0200

    Adding secure headers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  ensure_security_headers # See more: https://github.com/twitter/secureheaders
+
 end
