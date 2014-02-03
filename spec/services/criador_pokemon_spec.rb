8d994d44654fdb0b65ef0233f80514914a923c36
Mauro George <maurogot@gmail.com>
2014-02-03 17:51:49 -0200

    Adiciona o vcr no CriadorPokemon

--- spec/services/criador_pokemon_spec.rb
+++ spec/services/criador_pokemon_spec.rb
@@ -4,26 +4,24 @@ describe CriadorPokemon do
 
   describe '#criar' do
 
-    before do
-      arquivo_resposta = File.new('spec/fixtures/services/criador_pokemon/resposta.txt')
-      stub_request(:get, 'http://pokeapi.co/api/v1/pokemon/6/')
-      .to_return(arquivo_resposta)
-    end
-
     let(:criador_pokemon) do
       CriadorPokemon.new(6)
     end
 
     it 'cria um novo pokemon' do
       expect do
-        criador_pokemon.criar
+        VCR.use_cassette('CriadorPokemon/criar') do
+          criador_pokemon.criar
+        end
       end.to change{ Pokemon.count }.by(1)
     end
 
     describe 'pokemon criado' do
 
       before do
-        criador_pokemon.criar
+        VCR.use_cassette('CriadorPokemon/criar') do
+          criador_pokemon.criar
+        end
       end
 
       subject do
