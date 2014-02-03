9d1de1164430bd94ad0cd80862880208aa263424
Mauro George <maurogot@gmail.com>
2014-02-03 08:19:13 -0200

    Adiciona o WebMock as specs

--- spec/services/criador_pokemon_spec.rb
+++ spec/services/criador_pokemon_spec.rb
@@ -4,6 +4,14 @@ describe CriadorPokemon do
 
   describe '#criar' do
 
+    before do
+      body = '{' \
+        '  "name": "Charizard"' \
+        '}'
+      stub_request(:get, 'http://pokeapi.co/api/v1/pokemon/6/')
+      .to_return(status: 200, body: body, headers: {})
+    end
+
     let(:criador_pokemon) do
       CriadorPokemon.new(6)
     end
