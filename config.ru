ed3c578e9bb2c0492caec9f4eb16cab7bb2271b0
Mauro George <maurogot@gmail.com>
2014-02-03 07:45:23 -0200

    Configuring Unicorn.

--- config.ru
+++ config.ru
@@ -1,5 +1,22 @@
 # This file is used by Rack-based servers to start the application.
 
+# Run GC each 10 requests
+require "unicorn/oob_gc"
+use Unicorn::OobGC, 10
+
+# Unicorn Worker Killer Configuration:
+#
+require "unicorn/worker_killer"
+
+# Max requests per worker
+max_request_min =  ENV['MAX_REQUEST_MIN'].to_i || 3072
+max_request_max =  ENV['MAX_REQUEST_MAX'].to_i || 4096
+use Unicorn::WorkerKiller::MaxRequests, max_request_min, max_request_max
+
+# Max memory size (RSS) per worker
+oom_min = ((ENV['OOM_MIN'].to_i || 192) * (1024**2))
+oom_max = ((ENV['OOM_MAX'].to_i || 216) * (1024**2))
+use Unicorn::WorkerKiller::Oom, oom_min, oom_max
 require ::File.expand_path('../config/environment',  __FILE__)
 #Run heroku config:add CANONICAL_HOST=yourdomain.com
 #For more information, see: https://github.com/tylerhunt/rack-canonical-host#usage
---
+++ config/unicorn.rb
@@ -0,0 +1,43 @@
+# config/unicorn.rb
+
+worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
+timeout Integer(ENV['WEB_TIMEOUT'] || 15)
+
+# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
+# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
+
+# Enable this flag to have unicorn test client connections by writing the
+# beginning of the HTTP headers before calling the application.  This
+# prevents calling the application for connections that have disconnected
+# while queued.  This is only guaranteed to detect clients on the same
+# host unicorn runs on, and unlikely to detect disconnects even on a
+# fast LAN.
+check_client_connection false
+
+# Listen only on Heroku TCP port.
+# We use a shorter backlog for quicker failover when busy.
+listen ENV['PORT'], :backlog => Integer(ENV['UNICORN_BACKLOG'] || 200), :tcp_nopush => true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  GC.disable
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
