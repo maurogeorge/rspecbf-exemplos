b0fb0f7204c25163a08ad11183fdca9036ee7f41
Mauro George <maurogot@gmail.com>
2014-02-06 19:14:21 -0200

    Criada a factory de artigo

---
+++ spec/factories/artigos.rb
@@ -0,0 +1,8 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :artigo do
+    titulo 'Diversas dicas do RSpec'
+    conteudo 'Contenteúdo de Diversas dicas do RSpec'
+  end
+end
