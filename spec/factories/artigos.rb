003a8925eb65fc1ba75cf5a1c256b9b0b1ab9cba
Mauro George <maurogot@gmail.com>
2014-02-06 19:31:57 -0200

    Utilizando in line sequence na classe de artigo

--- spec/factories/artigos.rb
+++ spec/factories/artigos.rb
@@ -1,13 +1,8 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-
-  sequence :titulo do |n|
-    "Diversas dicas do RSpec #{n}"
-  end
-
   factory :artigo do
-    titulo
+    sequence(:titulo, 'a') { |n| "Diversas dicas do RSpec #{n}" }
     conteudo { "Conteudo do artigo #{titulo}. Approved: #{aprovado}" }
   end
 end
