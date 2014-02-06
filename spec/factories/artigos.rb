09f1c7b871a33234bb070571501c57a05c02c9d8
Mauro George <maurogot@gmail.com>
2014-02-06 19:15:57 -0200

    Utilizando herança na classe de artigo

--- spec/factories/artigos.rb
+++ spec/factories/artigos.rb
@@ -4,5 +4,13 @@ FactoryGirl.define do
   factory :artigo do
     titulo 'Diversas dicas do RSpec'
     conteudo 'Contenteúdo de Diversas dicas do RSpec'
+
+    factory :artigo_aprovado do
+      aprovado true
+    end
+
+    factory :artigo_nao_aprovado do
+      aprovado false
+    end
   end
 end
