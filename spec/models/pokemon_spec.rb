429215ca78e19b9f74f35adbb71ec1e0a142c87d
Mauro George <maurogot@gmail.com>
2014-02-06 19:03:28 -0200

    Adiciona fixtures de pokemon

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Pokemon do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :pokemons
+
+  describe '#nome_completo' do
+    it 'exibe o nome e o id nacional' do
+      pokemon = pokemons(:charizard)
+      expect(pokemon.nome_completo).to eq('Charizard - 6')
+    end
+  end
 end
