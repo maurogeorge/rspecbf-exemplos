22b0bd3e71ed7c91cd4e04d897043cdf69b93449
Mauro George <maurogot@gmail.com>
2014-02-06 18:59:41 -0200

    Cria o model Pokemon

---
+++ app/models/pokemon.rb
@@ -0,0 +1,2 @@
+class Pokemon < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140206205745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pokemons", force: true do |t|
+    t.integer  "id_nacional"
+    t.string   "nome"
+    t.integer  "ataque"
+    t.integer  "defesa"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
