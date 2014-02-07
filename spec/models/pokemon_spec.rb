b56dfe81901578fc309ea0c71d0ecb6cf4e545c9
Mauro George <maurogot@gmail.com>
2014-02-07 08:10:15 -0200

    Altera spec de Pokemon#nome_completo para usar factory

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -2,12 +2,18 @@ require 'spec_helper'
 
 describe Pokemon do
 
-  fixtures :pokemons
-
   describe '#nome_completo' do
+
+    let(:pokemon) do
+      create(:pokemon)
+    end
+
+    subject do
+      pokemon.nome_completo
+    end
+
     it 'exibe o nome e o id nacional' do
-      pokemon = pokemons(:charizard)
-      expect(pokemon.nome_completo).to eq('Charizard - 6')
+      expect(subject).to eq('Charizard - 6')
     end
   end
 end
