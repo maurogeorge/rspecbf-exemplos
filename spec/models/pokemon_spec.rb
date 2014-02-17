2b66965498a63d7f4c106b9ac2b9d9bbeecd2e01
Mauro George <maurogot@gmail.com>
2014-02-17 19:21:48 -0300

    Utilizando describe para testar o Pokemon#nome_completo

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe Pokemon do
 
-  it 'exibe o nome e o id nacional' do
-    pokemon = Pokemon.new(nome: 'Charizard', id_nacional: 6)
-    expect(pokemon.nome_completo).to eq('Charizard - 6')
+  describe '#nome_completo' do
+    it 'exibe o nome e o id nacional' do
+      pokemon = Pokemon.new(nome: 'Charizard', id_nacional: 6)
+      expect(pokemon.nome_completo).to eq('Charizard - 6')
+    end
   end
 end
