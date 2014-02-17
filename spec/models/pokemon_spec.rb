e47f897c829eda056d08a34dead4852c06ead966
Mauro George <maurogot@gmail.com>
2014-02-17 19:29:43 -0300

    Utilizando context em Pokemon#nome_completo

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -4,14 +4,20 @@ describe Pokemon do
 
   describe '#nome_completo' do
 
-    it 'exibe o nome e o id nacional quando possui os valores' do
-      pokemon = Pokemon.new(nome: 'Charizard', id_nacional: 6)
-      expect(pokemon.nome_completo).to eq('Charizard - 6')
+   context 'quando possui nome e o id nacional' do
+
+      it 'exibe o nome e o id nacional' do
+        pokemon = Pokemon.new(nome: 'Charizard', id_nacional: 6)
+        expect(pokemon.nome_completo).to eq('Charizard - 6')
+      end
     end
 
-    it 'é nil quando não possui o nome e o id nacional' do
-      pokemon = Pokemon.new
-      expect(pokemon.nome_completo).to be_nil
+    context 'quando não possui o nome e o id nacional' do
+
+      it 'é nil' do
+        pokemon = Pokemon.new
+        expect(pokemon.nome_completo).to be_nil
+      end
     end
   end
 end
