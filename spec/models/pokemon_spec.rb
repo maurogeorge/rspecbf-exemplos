2b26fdc3430565cc4f638ef7c76654a96b016826
Mauro George <maurogot@gmail.com>
2014-02-17 19:28:00 -0300

    Utilizando apenas uma asserção por it

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -3,11 +3,15 @@ require 'spec_helper'
 describe Pokemon do
 
   describe '#nome_completo' do
-    it 'exibe o nome e o id nacional' do
+
+    it 'exibe o nome e o id nacional quando possui os valores' do
       pokemon = Pokemon.new(nome: 'Charizard', id_nacional: 6)
-      pokemon_2 = Pokemon.new
       expect(pokemon.nome_completo).to eq('Charizard - 6')
-      expect(pokemon_2.nome_completo).to be_nil
+    end
+
+    it 'é nil quando não possui o nome e o id nacional' do
+      pokemon = Pokemon.new
+      expect(pokemon.nome_completo).to be_nil
     end
   end
 end
