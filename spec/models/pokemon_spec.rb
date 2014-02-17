f7125c47e7ff10d94a17fa1dcf5cafdf3485ff59
Mauro George <maurogot@gmail.com>
2014-02-17 19:33:06 -0300

    Utilizando subject

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -6,23 +6,23 @@ describe Pokemon do
 
    context 'quando possui nome e o id nacional' do
 
-     let(:pokemon) do
+     subject do
        Pokemon.new(nome: 'Charizard', id_nacional: 6)
      end
 
       it 'exibe o nome e o id nacional' do
-        expect(pokemon.nome_completo).to eq('Charizard - 6')
+        expect(subject.nome_completo).to eq('Charizard - 6')
       end
     end
 
     context 'quando não possui o nome e o id nacional' do
 
-      let(:pokemon) do
+      subject do
         Pokemon.new
       end
 
       it 'é nil' do
-        expect(pokemon.nome_completo).to be_nil
+        expect(subject.nome_completo).to be_nil
       end
     end
   end
