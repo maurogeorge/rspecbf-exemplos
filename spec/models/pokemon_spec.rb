64acdfc7a1bcf24b2bf87dae36ce9f7fcdd2b13b
Mauro George <maurogot@gmail.com>
2014-02-17 19:20:55 -0300

    Cria o teste de Pokemon#nome_completo utilizando apenas 1 asserção

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -2,10 +2,8 @@ require 'spec_helper'
 
 describe Pokemon do
 
-  describe '#nome_completo' do
-    it 'exibe o nome e o id nacional' do
-      pokemon = Pokemon.new(nome: 'Charizard', id_nacional: 6)
-      pokemon.nome_completo.should eq('Charizard - 6')
-    end
+  it 'exibe o nome e o id nacional' do
+    pokemon = Pokemon.new(nome: 'Charizard', id_nacional: 6)
+    expect(pokemon.nome_completo).to eq('Charizard - 6')
   end
 end
