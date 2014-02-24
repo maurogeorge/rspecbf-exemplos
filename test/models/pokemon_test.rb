02a7cb52e78e5c680e71107382b279ddab11f37f
Mauro George <maurogot@gmail.com>
2014-02-24 19:06:42 -0300

    Removendo a pasta test

--- test/models/pokemon_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PokemonTest < ActiveSupport::TestCase
-
-  test 'exibe o nome e o id nacional' do
-    pokemon = Pokemon.new(nome: 'Charizard', id_nacional: 6)
-    assert_equal 'Charizard - 6', pokemon.nome_completo
-  end
-end
