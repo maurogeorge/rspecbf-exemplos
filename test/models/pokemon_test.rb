4df40f1c5f1dbad7c3056236671075ea0ebad18f
Mauro George <maurogot@gmail.com>
2014-02-24 19:03:38 -0300

    Utilizando o minitest via método test

--- test/models/pokemon_test.rb
+++ test/models/pokemon_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PokemonTest < ActiveSupport::TestCase
 
-  def test_exibe_o_nome_e_o_id_nacional
+  test 'exibe o nome e o id nacional' do
     pokemon = Pokemon.new(nome: 'Charizard', id_nacional: 6)
     assert_equal 'Charizard - 6', pokemon.nome_completo
   end
