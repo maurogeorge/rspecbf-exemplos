d2fad574ccdda5cd4c43bc9b71ebda98af45b689
Mauro George <maurogot@gmail.com>
2014-03-07 18:28:46 -0300

    Usando o Timecop sem o bloco

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -4,36 +4,35 @@ describe Pokemon do
 
   describe '.escolhidos_ontem' do
 
-    let(:hoje) do
-      Time.zone.local(2010, 3, 7, 12)
-    end
-
     subject do
       Pokemon.escolhidos_ontem
+    end
+
+    before do
+      hoje = Time.zone.local(2010, 3, 7, 12)
+      Timecop.freeze(hoje)
+    end
+
+    after do
+      Timecop.return
     end
 
     it 'tem o pokemon escolhido ontem' do
       pokemon_escolhido_ontem = create(:pokemon,
                                        escolhido_em: Time.zone.local(2010, 3, 6, 23, 59, 59))
-      Timecop.freeze(hoje) do
-        expect(subject).to include(pokemon_escolhido_ontem)
-      end
+      expect(subject).to include(pokemon_escolhido_ontem)
     end
 
     it 'não tem o pokemon escolhido hoje' do
       pokemon_escolhido_hoje = create(:pokemon,
                                       escolhido_em: Time.zone.local(2010, 3, 7, 15))
-      Timecop.freeze(hoje) do
-        expect(subject).to_not include(pokemon_escolhido_hoje)
-      end
+      expect(subject).to_not include(pokemon_escolhido_hoje)
     end
 
     it 'não tem o pokemon escolhido antes de ontem' do
       pokemon_escolhido_antes_de_ontem = create(:pokemon,
                                                 escolhido_em: Time.zone.local(2010, 3, 5))
-      Timecop.freeze(hoje) do
-        expect(subject).to_not include(pokemon_escolhido_antes_de_ontem)
-      end
+      expect(subject).to_not include(pokemon_escolhido_antes_de_ontem)
     end
   end
 end
