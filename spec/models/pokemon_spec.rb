d560e3b3d6e07bb9f18f11ec0028c0208d3a5d59
Mauro George <maurogot@gmail.com>
2014-03-07 18:21:02 -0300

    Removendo alguns lets

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -8,35 +8,29 @@ describe Pokemon do
       Time.zone.local(2010, 3, 7, 12)
     end
 
-    let!(:pokemon_escolhido_hoje) do
-      create(:pokemon, escolhido_em: Time.zone.local(2010, 3, 7, 15))
-    end
-
-    let!(:pokemon_escolhido_ontem) do
-      create(:pokemon, escolhido_em: Time.zone.local(2010, 3, 6, 23, 59, 59))
-    end
-
-    let!(:pokemon_escolhido_antes_de_ontem) do
-      create(:pokemon, escolhido_em: Time.zone.local(2010, 3, 5))
-    end
-
     subject do
       Pokemon.escolhidos_ontem
     end
 
     it 'tem o pokemon escolhido ontem' do
+      pokemon_escolhido_ontem = create(:pokemon,
+                                       escolhido_em: Time.zone.local(2010, 3, 6, 23, 59, 59))
       Timecop.freeze(hoje) do
         expect(subject).to include(pokemon_escolhido_ontem)
       end
     end
 
-    it 'não tem o pokemon hoje' do
+    it 'não tem o pokemon escolhido hoje' do
+      pokemon_escolhido_hoje = create(:pokemon,
+                                      escolhido_em: Time.zone.local(2010, 3, 7, 15))
       Timecop.freeze(hoje) do
         expect(subject).to_not include(pokemon_escolhido_hoje)
       end
     end
 
     it 'não tem o pokemon escolhido antes de ontem' do
+      pokemon_escolhido_antes_de_ontem = create(:pokemon,
+                                                escolhido_em: Time.zone.local(2010, 3, 5))
       Timecop.freeze(hoje) do
         expect(subject).to_not include(pokemon_escolhido_antes_de_ontem)
       end
