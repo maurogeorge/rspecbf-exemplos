07cbb5e5e6772dadc98d9c352b31f27d19bbc1e1
Mauro George <maurogot@gmail.com>
2014-03-12 07:35:14 -0300

    Exibe coverage com menos de 100%

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -37,29 +37,4 @@ describe Pokemon do
       expect(subject).to_not include(pokemon_escolhido_antes_de_ontem)
     end
   end
-
-  describe '#nome_completo' do
-
-   context 'quando possui nome e o id nacional' do
-
-     subject(:pokemon) do
-       Pokemon.new(nome: 'Charizard', id_nacional: 6)
-     end
-
-      it 'exibe o nome e o id nacional' do
-        expect(pokemon.nome_completo).to eq('Charizard - 6')
-      end
-    end
-
-    context 'quando não possui o nome e o id nacional' do
-
-      subject do
-        Pokemon.new
-      end
-
-      it 'é nil' do
-        expect(subject.nome_completo).to be_nil
-      end
-    end
-  end
 end
