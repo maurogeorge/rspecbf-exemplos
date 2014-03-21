246017eb0f74e5aeea09af624935aa928b95ff93
Mauro George <maurogot@gmail.com>
2014-03-21 08:09:07 -0300

    Adiciona validação em Pokemon#id_nacional

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,5 @@
 class Pokemon < ActiveRecord::Base
 
   validates :nome, :id_nacional, presence: true
+  validates :id_nacional, numericality: { only_integer: true, greater_than: 0 }
 end
--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -6,5 +6,6 @@ describe Pokemon do
 
     it { should validate_presence_of(:nome) }
     it { should validate_presence_of(:id_nacional) }
+    it { should validate_numericality_of(:id_nacional).only_integer.is_greater_than(0) }
   end
 end
