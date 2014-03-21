5b77d9b1814f08918cd9e0671521dc5e358bb41e
Mauro George <maurogot@gmail.com>
2014-03-21 08:27:42 -0300

    Criado um grupo para o teste do id_nacional

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -5,7 +5,11 @@ describe Pokemon do
   describe 'validações' do
 
     it { should validate_presence_of(:nome) }
-    it { should validate_presence_of(:id_nacional) }
-    it { should validate_numericality_of(:id_nacional).only_integer.is_greater_than(0) }
+
+    describe 'id_nacional' do
+
+      it { should validate_presence_of(:id_nacional) }
+      it { should validate_numericality_of(:id_nacional).only_integer.is_greater_than(0) }
+    end
   end
 end
