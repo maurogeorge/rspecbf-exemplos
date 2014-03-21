160d6106c2b2507221223c1a4a739ed31e5eeeae
Mauro George <maurogot@gmail.com>
2014-03-21 08:28:34 -0300

    Adiciona validação de Pokemon#ataque

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,4 +2,5 @@ class Pokemon < ActiveRecord::Base
 
   validates :nome, :id_nacional, presence: true
   validates :id_nacional, numericality: { only_integer: true, greater_than: 0 }
+  validates :ataque, presence: true, if: :aprovado?
 end
--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -11,5 +11,26 @@ describe Pokemon do
       it { should validate_presence_of(:id_nacional) }
       it { should validate_numericality_of(:id_nacional).only_integer.is_greater_than(0) }
     end
+
+    describe 'ataque' do
+
+      context 'quando está aprovado' do
+
+        subject do
+          Pokemon.new(aprovado: true )
+        end
+
+        it { should validate_presence_of(:ataque) }
+      end
+
+      context 'quando não está aprovado' do
+
+        subject do
+          Pokemon.new
+        end
+
+        it { should_not validate_presence_of(:ataque) }
+      end
+    end
   end
 end
