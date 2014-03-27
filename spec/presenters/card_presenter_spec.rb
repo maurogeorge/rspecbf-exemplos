27d882e4abba009a06accd310c47cd85def19130
Mauro George <maurogot@gmail.com>
2014-03-27 19:55:46 -0300

    Utilizando do atalho de uma linha

--- spec/presenters/card_presenter_spec.rb
+++ spec/presenters/card_presenter_spec.rb
@@ -8,15 +8,15 @@ describe CardPresenter do
       CardPresenter.new(objeto)
     end
 
-    before do
-      to_presenter = { nome: 'Mauro', idade: 24 }
-      allow(objeto).to receive(:to_presenter).and_return(to_presenter)
-    end
-
     context 'comum' do
 
       let(:objeto) do
         double('Um objeto')
+      end
+
+      before do
+        to_presenter = { nome: 'Mauro', idade: 24 }
+        allow(objeto).to receive(:to_presenter).and_return(to_presenter)
       end
 
       it 'retorna um paragrafo por chave' do
@@ -27,11 +27,11 @@ describe CardPresenter do
     context 'Pokemon' do
 
       let(:objeto) do
-        object_double(Pokemon.new)
+        object_double(Pokemon.new, to_presenter: { nome: 'Chazirard' })
       end
 
       it 'retorna um paragrafo por chave' do
-        expect(card_presenter.show).to eq(%{<p>nome: Mauro</p><p>idade: 24</p>})
+        expect(card_presenter.show).to eq(%{<p>nome: Chazirard</p>})
       end
     end
   end
