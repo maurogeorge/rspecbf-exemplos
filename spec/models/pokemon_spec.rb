a7aa84d12de8bac083678822c019a5280a973e0b
Mauro George <maurogot@gmail.com>
2014-04-02 19:05:08 -0300

    Adicionar valor que o stub deve receber

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -5,7 +5,7 @@ describe Pokemon do
   describe '#ataque_critico' do
 
     it 'é um valor aleatório' do
-      allow(Random).to receive(:rand).and_return(75)
+      allow(Random).to receive(:rand).with(60..80).and_return(75)
       pokemon = Pokemon.new
       expect(pokemon.ataque_critico).to eq(75)
     end
