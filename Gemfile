2e9bbc3123f2238629f73e1ca0a8f3bb97fe6556
Mauro George <maurogot@gmail.com>
2014-05-26 18:32:07 -0300

    Atualizando o Rails para o 4.1.1

--- Gemfile
+++ Gemfile
@@ -1,21 +1,21 @@
 source 'https://rubygems.org'
 ruby '2.1.0'
 
-gem 'rails',                  '4.0.2'
+gem 'rails',                  '4.1.1'
 gem 'unicorn',                '4.7.0'
 gem 'unicorn-worker-killer',  '0.4.2'
 gem 'secure_headers',         '1.1.0'
 gem 'jquery-rails',           '3.0.4'
 gem 'turbolinks',             '2.0.0'
 gem 'jbuilder',               '1.5.3'
-gem 'haml-rails',             '0.5.2'
+gem 'haml-rails',             '0.5.3'
 gem 'pg',                     '0.17.0'
 gem 'sass-rails',             '4.0.1'
 gem 'coffee-rails',           '4.0.1'
 gem 'uglifier',               '2.3.2'
 gem 'rack-canonical-host',    '0.0.8'
 gem 'bourbon',                '3.1.8'
-gem 'simple_form',            '3.0.1'
+gem 'simple_form',            '3.0.2'
 
 group :production do
   gem 'rails_12factor',       '0.0.2'
@@ -31,7 +31,7 @@ group :development do
 end
 
 group :test do
-  gem 'shoulda-matchers',     '2.4.0'
+  gem 'shoulda-matchers',     '2.6.1'
   gem 'simplecov',            '0.8.2', require: false
   gem 'email_spec',           '1.5.0'
   gem 'capybara',             '2.1.0'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-RspecbfExemplos::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -19,7 +19,7 @@ RspecbfExemplos::Application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations
+  # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
@@ -30,4 +30,11 @@ RspecbfExemplos::Application.configure do
   # Letter openner config
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 end
